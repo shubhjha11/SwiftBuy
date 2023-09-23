72bd523c55ea9dc4933e59256c98085346bdf151
Shubham Jhanwar <shubhjhan123@gmail.com>
2023-09-23 18:39:15 +0530

    Styling changes

--- src/package/modules/dashboard/popular-category/popular-category.component.ts
+++ src/package/modules/dashboard/popular-category/popular-category.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopularCategoryComponent implements OnInit {
 
+  public categories: any[] = [];
   constructor() { }
 
   ngOnInit(): void {
