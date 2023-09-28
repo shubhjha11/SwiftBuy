4bbed15fc11897543b3d132c4a51916ecd034276
Shubham Jhanwar <shubhjhan123@gmail.com>
2023-09-28 17:58:44 +0530

    scroll to top and breakpoint variable

--- src/package/core/layout/layout.component.ts
+++ src/package/core/layout/layout.component.ts
@@ -10,6 +10,29 @@ export class LayoutComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    window.addEventListener('scroll', this.scroll, true);
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.scroll, true);
+  }
+
+  public scroll = (): void => {
+    const scrollPosition = window.pageYOffset;
+    const scrollButton = document.getElementById('scrollButton') as HTMLElement;
+    if (scrollPosition > 250) {
+      scrollButton.classList.add('active');
+    } else {
+      scrollButton.classList.remove('active');
+    }
+  }
+
+  public scrollToTop(): void {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
   }
 
 }
